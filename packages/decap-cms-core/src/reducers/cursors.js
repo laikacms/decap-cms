3569758345f7cfa8eabc90ee52da858c8c0c3bef
Sem Postma <sem@esstudio.nl>
2026-07-14 20:47:16 +0200

    refactor(core): remove Immutable.js from cursors reducer (DCMS-468) (#678)
    Co-authored-by: pink-worker-decap-cms-bot <pink-worker-decap-cms-bot@openclaw.bot>

--- packages/decap-cms-core/src/reducers/cursors.js
+++ packages/decap-cms-core/src/reducers/cursors.js
@@ -1,4 +1,3 @@
-import { fromJS } from 'immutable';
 import { Cursor } from 'decap-cms-lib-util';
 
 import {
@@ -12,21 +11,35 @@ import {
 // and searches are the most common examples), we namespace cursors by
 // their type before storing them in the state.
 export function selectCollectionEntriesCursor(state, collectionName) {
-  return new Cursor(state.getIn(['cursorsByType', 'collectionEntries', collectionName]));
+  return new Cursor(state.cursorsByType?.collectionEntries?.[collectionName]);
 }
 
-function cursors(state = fromJS({ cursorsByType: { collectionEntries: {} } }), action) {
+function cursors(state = { cursorsByType: { collectionEntries: {} } }, action) {
   switch (action.type) {
     case ENTRIES_SUCCESS: {
-      return state.setIn(
-        ['cursorsByType', 'collectionEntries', action.payload.collection],
-        Cursor.create(action.payload.cursor).store,
-      );
+      return {
+        ...state,
+        cursorsByType: {
+          ...state.cursorsByType,
+          collectionEntries: {
+            ...state.cursorsByType?.collectionEntries,
+            [action.payload.collection]: Cursor.create(action.payload.cursor).store,
+          },
+        },
+      };
     }
     case FILTER_ENTRIES_SUCCESS:
     case GROUP_ENTRIES_SUCCESS:
     case SORT_ENTRIES_SUCCESS: {
-      return state.deleteIn(['cursorsByType', 'collectionEntries', action.payload.collection]);
+      const { [action.payload.collection]: _, ...collectionEntries } =
+        state.cursorsByType?.collectionEntries ?? {};
+      return {
+        ...state,
+        cursorsByType: {
+          ...state.cursorsByType,
+          collectionEntries,
+        },
+      };
     }
     default:
       return state;
